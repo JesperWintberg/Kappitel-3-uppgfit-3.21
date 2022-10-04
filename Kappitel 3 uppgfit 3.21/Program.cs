b2a0944b50f595dc93a1809ba473318b5c4ad33a
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-10-04 20:06:16 +0200

    Add project files.

---
+++ Kappitel 3 uppgfit 3.21/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en addition");
+            string[] addition = Console.ReadLine().Split('+');
+
+            int[] tal = Array.ConvertAll(addition, int.Parse);
+
+            int summan = tal.Sum();
+
+            Console.WriteLine($"Summan blir då {summan}.");
+        }
+    }
+}
+
